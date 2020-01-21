3658fe604d7462e0fdbaea6a4397a9a71781b80f
Robert <rbmanez@gmail.com>
2020-01-21 16:31:45 -0700

    Updates

---
+++ precache-manifest.1c69d3dd227012e6375f79c88577d944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ff4dc23758b2ed70383eb57a807013",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "30165c69f403257e8ac0",
+    "url": "/robofriends/static/css/main.99ffd9da.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30165c69f403257e8ac0",
+    "url": "/robofriends/static/js/main.79a5c908.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
